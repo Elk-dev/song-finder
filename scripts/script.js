e153ee975162598b32078b108d8730f899408984
Elkin Huertas <mauricioelkin@live.com>
2020-05-06 13:39:29 -0400

    map use of api song search

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,32 @@
 const url = 'https://itunes.apple.com/search?term=beyonce';
 fetch(url)
 .then( (response) => response.json() )
-.then( (data) => console.log(data.results) )
+.then( (data) => {
+    // console.log(data.results); 
+    const artists = data.results;
+    return artists.map(result => {
+         const songContainer = document.getElementById('songs')
+
+         const article =  document.createElement('article'),
+               artist =  document.createElement('p'),
+               song =  document.createElement('p'),
+               img =  document.createElement('img'),
+               audio =  document.createElement('audio'),
+               audioSource =  document.createElement('source')
+
+                artist.innerHTML = result.artistName
+                song.innerHTML = result.trackName
+                img.src = result.artworkUrl100
+                audioSource.src = result.previewUrl
+                audio.setAttribute('controls', '')
+
+                article.appendChild(img)
+                article.appendChild(artist)
+                article.appendChild(song)
+                article.appendChild(audio)
+                audio.appendChild(audioSource)
+                songContainer.appendChild(article)
+    })
+})
 
 .catch(error => console.log('Request failed: ', error))

bd76b426aecb3e1d8b6b136828cfaf2f7c4804e3
Elkin Huertas <mauricioelkin@live.com>
2020-05-06 14:51:39 -0400

    finished song finder webpage

--- scripts/script.js
+++ scripts/script.js
@@ -50,3 +50,11 @@ const updateTerm = () => {
 const searchBtn = document.querySelector('button')
 searchBtn.addEventListener('click', updateTerm)
 
+document.addEventListener('play', event => {
+    const audio = document.getElementsByTagName('audio');
+    for(let i = 0; i < audio.length; i++){
+        if(audio[i] !=event.target) {
+            audio[i].pause();
+        }
+    }
+}, true)
